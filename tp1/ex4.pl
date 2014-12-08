2e585ec871c0348a08f8d4f60718b9967325455e
Thomas Poulet <dark.flames0@gmail.com>
2014-12-08 19:01:25 +0100

    Update ex4.pl

--- tp1/ex4.pl
+++ tp1/ex4.pl
@@ -1,3 +1,7 @@
+%% Prédicats
+% Titre : Definition du predicat arc
+% Prédicat : arc/2
+% Usage : arc(X, Y) caractérise un arc allant de X à Y
 arc(bachant, aulnoye).
 arc(bachant, croix).
 arc(bachant, denain).
@@ -9,21 +13,50 @@ arc(friche, gommegnies).
 arc(croix, denain).
 arc(denain, gommegnies).
 
+%% Règles
+% Titre : Definition de la règle voisin
+% Règle : voisin/2
+% Usage : voisin(X, Y) permet de passer outre la directionnalité de la règle arc
 voisin(X,Y):- arc(X,Y).
 voisin(X,Y):- arc(Y,X).
 
+% Titre : Definition de la règle itinéraire
+% Règle : itineraire/2
+% Usage : itineraire(X, Y) permet de déterminer si un itineraire est possible
+% Param 1 : Départ
+% Param 2 : Arrivée
 itineraire(Start, End):- arc(Start, End).
 itineraire(Start, End):- arc(Start, Z), itineraire(Z, End).
 
+% Titre : Definition de la règle chemin
+% Règle : chemin/3
+% Usage : chemin(X, Y) permet d'obtenir la liste des chemins entre deux points
+% Param 1 : Départ
+% Param 2 : Arrivée
+% Param 3 : Liste résultat
 chemin(Start,End,List) :- arc(Start,End), append([Start],[End],List).
 chemin(Start,End,List) :- arc(Start,Etape), chemin(Etape,End,List1), append([Start],List1,List).
 
+% Titre : Definition de la règle cheminTailleFixe
+% Règle : cheminTailleFixe/4
+% Usage : cheminTailleFixe(X, Y) permet d'obtenir la liste des chemins de taille exact entre deux points
+% Param 1 : Départ
+% Param 2 : Arrivée
+% Param 3 : Liste résultat
+% Param 4 : Taille limite
 cheminTailleFixe(Start,End,List,Taille):-
 	chemin(Start,End, List1),
 	length(List1, L),
 	L-1 =:= Taille,
 	append(List1,[],List).
 
+% Titre : Definition de la règle cheminTailleMin
+% Règle : cheminTailleMin/4
+% Usage : cheminTailleFixe(X, Y) permet d'obtenir la liste des chemins de respectant une taille minimal entre deux points
+% Param 1 : Départ
+% Param 2 : Arrivée
+% Param 3 : Liste résultat
+% Param 4 : Taille limite
 cheminTailleMin(Start,End,List,Taille):-
 	chemin(Start,End, List1),
 	length(List1, L),
