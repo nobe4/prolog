401f98bd39a9c6eed307d2952969dd23d327e9fa
Thomas Poulet <dark.flames0@gmail.com>
2014-12-08 19:08:01 +0100

    Update ex3.pl

--- tp1/ex3.pl
+++ tp1/ex3.pl
@@ -1,67 +1,67 @@
-%% Prédicats sur les listes
-% Titre : Definition du predicat sommeList
-% Prédicat : sommeList/2
-% Parametre 1 : Liste dont on cherche a sommer les elements    
-% Parametre 2 : Resultat
+%% Règles sur les listes
+% Titre : Définition de la règle sommeList
+% Règle : sommeList/2
+% Param 1 : Liste dont on cherche à sommer les éléments    
+% Param 2 : Résultat
 % Usage : Calcul de la somme des elements de la liste.
 sommeList([],0):- !.
 sommeList([L|T], Somme) :- sommeList(T,S), Somme is S + L.
 
-% Titre : Definition du predicat sommeImpaire
-% Prédicat : sommeList/2
-% Parametre 1 : Liste dont on cherche a sommer les elements    
-% Parametre 2 : Resultat
-% Usage : Calcul de la somme des elements aux positions impairs de la liste (1, 3, ...).
+% Titre : Définition de la règle sommeImpaire
+% Règle : sommeList/2
+% Param 1 : Liste dont on cherche à sommer les éléments    
+% Param 2 : Résultat
+% Usage : Calcul de la somme des éléments aux positions impairs de la liste (1, 3, ...).
 sommeImpaire([],0) :- !.
 sommeImpaire([W,_|T],Somme) :- sommeImpaire(T,S), Somme is W + S.
 sommeImpaire([W],Somme) :- Somme is W.
 
-% Titre : Definition du predicat moyenne
-% Prédicat : moyenne/2
-% Parametre 1 : Liste dont on cherche a calculer la moyenne    
-% Parametre 2 : Resultat
-% Usage : Calcul la moyenne des elements.
+% Titre : Définition de la règle moyenne
+% Règle  : moyenne/2
+% Param 1 : Liste dont on cherche à calculer la moyenne    
+% Param 2 : Résultat
+% Usage : Calcul la moyenne des éléments.
 moyenne(L,M):- sommeList(L,S), length(L,Len), M is S / Len.
 
-% Titre : Definition du predicat noccur
-% Prédicat : noccur/3
-% Parametre 1 : Element   
-% Parametre 2 : Liste dans laquelle on recherche le nombre d'occurence du parametre 1
-% Parametre 3 : Resultat
-% Usage : Calcul le nombre d'occurence du parametre 1.
+% Titre : Définition de la règle noccur
+% Règle : noccur/3
+% Param 1 : Elément   
+% Param 2 : Liste dans laquelle on recherche le nombre d'occurence du paramètre 1
+% Param 3 : Résultat
+% Usage : Calcul le nombre d'occurence du paramètre 1.
 noccur(_, [], 0).
 noccur(E,[E|T],X):- noccur(E, T, X2), X is X2 + 1 .
 noccur(E,[Y|T],X):- E \= Y, noccur(E, T, X).
 
-% Titre : Definition du predicat adjacent
-% Prédicat : adjacent/3
-% Parametre 1 : Element 1  
-% Parametre 2 : Element 2  
-% Parametre 3 : Liste dans laquelle on recherche l'adjacence des elements.
-% Usage : Recherche l'adjacence des parametres 1 et 2 (dans cet ordre) dans le parametre 3.
+% Titre : Définition de la règle adjacent
+% Règle : adjacent/3
+% Param 1 : Elément 1  
+% Param 2 : Elément 2  
+% Param : Liste dans laquelle on recherche l'adjacence des éléments.
+% Usage : Recherche l'adjacence des paramètres 1 et 2 (dans cet ordre) dans le paramètre 3.
 adjacent(X,Y,[Z|T]):- X \= Z, Y \= Z,  adjacent(X,Y,T).
 adjacent(X,Y,[X,Y|_]).
 
-% Titre : Definition du predicat prefix
-% Prédicat : prefix/2
-% Parametre 1 : Element 1  
-% Parametre 2 : Element 2
-% Usage : Recherche si le parametre 1 est un prefix du parametre 2. 
+% Titre : Définition de la règle prefix
+% Règle : prefix/2
+% Param 1 : Elément 1  
+% Param 2 : Elément 2
+% Usage : Recherche si le paramètre 1 est un prefix du paramètre 2. 
 prefix([],_).
 prefix([H|T],[H|U]):- prefix(T,U).
 
-% Titre : Definition du predicat suffix
-% Prédicat : suffix/2
-% Parametre 1 : Element 1  
-% Parametre 2 : Element 2
-% Usage : Recherche si le parametre 1 est un suffix du parametre 2. 
+% Titre : Définition de la règle suffix
+% Règle : suffix/2
+% Param 1 : Elément 1  
+% Param 2 : Elément 2
+% Usage : Recherche si le paramètre 1 est un suffix du paramètre 2. 
 suffix(S,S).
 suffix(S,[_|T]) :- suffix(S,T).
 
-% Titre : Definition du predicat double
-% Prédicat : double/2
-% Parametre 1 : Element 1  
-% Parametre 2 : Element 2
-% Usage : Recherche si le parametre 2 est un double du parametre 1. 
+% Titre : Définition de la règle double
+% Règle : double/2
+% Param 1 : Elément 1  
+% Param 2 : Elément 2
+% Usage : Recherche si le paramètre 2 est un double du paramètre 1. 
 double([T],[T,T]).
 double([T|L],[T,T|X]):- double(L,X).
