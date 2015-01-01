8ac08bc655e02dee0d169f619af3b0726b0fd822
Thomas Poulet <dark.flames0@gmail.com>
2015-01-01 15:13:33 +0100

    Update ex3.pl

--- tp2/ex3.pl
+++ tp2/ex3.pl
@@ -1,4 +1,4 @@
-% Predicat remplace/4
+% Regle remplace/4
 % Usage : remplace(X1, X2, L1, L2) construit la liste L2 en remplaçant tous les élément X1 par X2 dans la liste L2.
 % Param 1 : Element a remplacer
 % Param 2 : Element remplace
@@ -8,7 +8,7 @@ remplace(_,_,[],[]).
 remplace(X1,X2,[H|T1],[X2|T2]) :- H=X1, remplace(X1,X2,T1,T2).
 remplace(X1,X2,[H|T1],[H|T2]) :- remplace(X1,X2,T1,T2).
 
-% Predicat entier_liste/2
+% Regle entier_liste/2
 % Usage : entier_liste(N, LN) qui construit la liste LN comme les chiffres du nombre N
 % Param 1 : Nombre
 % Param 4 : Liste resultat
@@ -19,7 +19,7 @@ entier_liste(N,A) :-
     entier_liste(N1,As),
     append(As,[N2],A).
 
-% Predicat entier_liste/2
+% Regle inverse/2
 % Usage : inverse(N, LR) qui construit la liste LR en inversant la liste L
 % Param 1 : Liste a inverser
 % Param 4 : Liste resultat
@@ -28,7 +28,7 @@ inverse([H|T],R):-
     inverse(T,RevT),  
     append(RevT,[H],R).
 
-% Predicat palindromeNb/1
+% Regle palindromeNb/1
 % Usage : palindromeNb(L) indique si la liste L est un palindrome
 % Param 1 : Liste
 palindromeNb(L):-
