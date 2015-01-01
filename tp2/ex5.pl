fd9fde75c7a47673c9efbe77f9e3ed462a391519
Thomas Poulet <dark.flames0@gmail.com>
2015-01-01 15:12:17 +0100

    Update ex5.pl

--- tp2/ex5.pl
+++ tp2/ex5.pl
@@ -1,14 +1,17 @@
-% Regle : Decompose une chaine de caractere en tableau de caractere
+% Regle explode/2
+% Usage : Decompose une chaine de caractere en tableau de caractere
 % param 1 : Le mot a decomposer
 % param 2 : Le tableau resultat
 explode(C,R):- atom_chars(C,R).
 
-% Regle : Merge un tableau en une chaine de caractere
+% Regle implode/2
+% Usage : Merge un tableau en une chaine de caractere
 % param 1 : Le tableau a merger
 % param 2 : Le mot resultat
 implode(L,R):- atomic_list_concat(L,R).
 
-% Regle : Verifie si deux mots sont des anagrammes
+% Regle anagrame/2
+% Usage : Verifie si deux mots sont des anagrammes
 % param 1 : Le premier mot
 % param 2 : Le deuxieme mot
 anagrame(X,Y):-
@@ -18,14 +21,16 @@ anagrame(X,Y):-
 	sort(Yex, Ysort),
 	Xsort == Ysort.
 
-% Trouve la solution d'un cryptogramme
+% Regle solve/3
+% Usage : Trouve la solution d'un cryptogramme
 % param 1 : Le premier mot additionne
 % param 2 : Le deuxieme mot additionne
 % param 3 : Le resultat
 solve(Word1,Word2,Solution):-
 	somme(Word1,Word2,Solution,0,0,[0,1,2,3,4,5,6,7,8,9],_).
 
-% Regle utilitaire de somme d'element avec prise en compte de la retenue
+% Regle somme/7
+% Usage : utilitaire de somme d'element avec prise en compte de la retenue
 % param 1 : Le premier nombre additionne
 % param 2 : Le deuxieme nombre additionne
 % param 3 : Le resultat
@@ -38,7 +43,8 @@ somme([D1|N1],[D2|N2],[D|N],RetenueDroite,RetenueGauche,PossibleDigs,Digs):-
 	somme(N1,N2,N,RetenueDroite,C2,PossibleDigs,Digs2),
 	sommeChiffres(D1,D2,C2,D,RetenueGauche,Digs2,Digs).
 
-% Regle utilitaire de somme de deux chiffres avec prise en compte de la retenue
+% Regle sommeChiffres/7
+% Usage : Regle utilitaire de somme de deux chiffres avec prise en compte de la retenue
 % param 1 : Le premier chiffre additionne
 % param 2 : Le deuxieme chiffre additionne
 % param 3 : La retenue precedente droite
@@ -54,7 +60,8 @@ sommeChiffres(D1,D2,RetenueDroite,D,RetenueGauche,PossibleDigs,Digs):-
 	D is S mod 10,
 	RetenueGauche is S // 10.
 
-% Regle effectuant la suppresion d'un element au sein d'une liste
+% Regle supprimer/3
+% Usage : Regle effectuant la suppresion d'un element au sein d'une liste
 % param 1 : L'element a supprimer
 % param 2 : La liste de recherche
 % param 3 : La liste resultat
