b5f353154a1211698e4889acfdb41e27e19d092e
Victor Haffreingue <nobe4@users.noreply.github.com>
2015-01-01 14:06:00 +0100

    added comments

--- tp2/ex5.pl
+++ tp2/ex5.pl
@@ -25,19 +25,39 @@ anagrame(X,Y):-
 solve(Word1,Word2,Solution):-
 	somme(Word1,Word2,Solution,0,0,[0,1,2,3,4,5,6,7,8,9],_).
 
+% Regle utilitaire de somme d'element avec prise en compte de la retenue
+% param 1 : Le premier nombre additionne
+% param 2 : Le deuxieme nombre additionne
+% param 3 : Le resultat
+% param 4 : La retenue precedente droite
+% param 5 : La retenue precedente gauche
+% param 6 : Les valeurs possibles
+% param 6 : Les valeurs impossibles
 somme([],[],[],C,C,Digits,Digits).
-somme([D1|N1],[D2|N2],[D|N],RetenueDroite,RetenueGauche,Digs1,Digs):-
-	somme(N1,N2,N,RetenueDroite,C2,Digs1,Digs2),
+somme([D1|N1],[D2|N2],[D|N],RetenueDroite,RetenueGauche,PossibleDigs,Digs):-
+	somme(N1,N2,N,RetenueDroite,C2,PossibleDigs,Digs2),
 	sommeChiffres(D1,D2,C2,D,RetenueGauche,Digs2,Digs).
 
-sommeChiffres(D1,D2,RetenueDroite,D,RetenueGauche,Digs1,Digs):-
-	supprimer(D1,Digs1,Digs2),
+% Regle utilitaire de somme de deux chiffres avec prise en compte de la retenue
+% param 1 : Le premier chiffre additionne
+% param 2 : Le deuxieme chiffre additionne
+% param 3 : La retenue precedente droite
+% param 4 : Le resultat
+% param 5 : La retenue precedente gauche
+% param 6 : Les valeurs possibles
+% param 6 : Les valeurs impossibles
+sommeChiffres(D1,D2,RetenueDroite,D,RetenueGauche,PossibleDigs,Digs):-
+	supprimer(D1,PossibleDigs,Digs2),
 	supprimer(D2,Digs2,Digs3),
 	supprimer(D,Digs3,Digs),
 	S is D1+D2+RetenueDroite,
 	D is S mod 10,
 	RetenueGauche is S // 10.
 
+% Regle effectuant la suppresion d'un element au sein d'une liste
+% param 1 : L'element a supprimer
+% param 2 : La liste de recherche
+% param 3 : La liste resultat
 supprimer(A,L,L):- nonvar(A),!.
 supprimer(A,[A|L],L).
 supprimer(A,[B|L],[B|L1]):- supprimer(A,L,L1).
