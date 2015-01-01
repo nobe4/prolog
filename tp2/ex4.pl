e6c9260159eb08b75bb2dbf169925496c0ffd554
Thomas Poulet <dark.flames0@gmail.com>
2015-01-01 15:12:48 +0100

    Update ex4.pl

--- tp2/ex4.pl
+++ tp2/ex4.pl
@@ -1,10 +1,10 @@
-% Predicat queDesZeros/1
+% Regle queDesZeros/1
 % Usage : Regle verifiant qu'une liste ne contient que des 0
 % param 1 : La liste de recherche
 queDesZeros([T]):- T is 0.
 queDesZeros([T|L]):- T is 0, queDesZeros(L).
 
-% Predicat valeurEntier/2
+% Regle valeurEntier/2
 % Usage : Regle effectuant la convertion d'une liste de chiffres binaire en sa representation decimale
 % param 1 : La liste de chiffres binaires
 % param 2 : Le resultat decimal
@@ -16,7 +16,7 @@ valeurEntier([T|L], N):-
     T1 is T*P, 
     N is N1+T1.
     
-% Predicat valeurBinaire/2
+% Regle valeurBinaire/2
 % Usage : Regle effectuant la convertion d'un nombre decimale en sa representation binaire
 % param 1 : Le nombre decimal
 % param 2 : La liste resultat de chiffres binaires
